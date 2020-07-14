a5090c20fb36817f477736e3cdb74280cbcb4ca0
niccolopredazzer <59871675+niccolopredazzer@users.noreply.github.com>
2020-07-14 14:07:28 +0200

    Initialize TemplateView

---
+++ app/Http/Controllers/HouseController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\House;
+use App\Promotion;
+
+class HouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $houses = House::all();
+        // $promotions = Promotion::all();
+        return view('guest.index',compact('houses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $house = House::findOrFail($id);
+
+        return view('guest.show', compact('house'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/guest/index.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.layout')
+@section('content')
+
+    <div class="offset-1 col-10">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active" aria-current="page">Home</li>
+                <li class="breadcrumb-item active" aria-current="page">Pages</li>
+		    </ol>
+        </nav>
+
+        {{-- TUTTI POSSONO VEDERE LE CASE --}}
+        <div class="row">
+            <div class="col-5">
+                <h2>Pages</h2>
+            </div>
+        </div>
+        <table class="table">
+            <thead>
+                <th>id</th>
+                <th>Title</th>
+                <th colspan="3">Action</th>
+            </thead>
+            <tbody>
+                @foreach ($houses as $key => $house)
+                    <tr>
+                        <td>{{$house->id}}</td>
+                        <td>{{$house->title}}</td>
+                        <td><a class="btn btn-primary" href="{{route('houses.show', $house->id)}}">Visualizza</a></td>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+@endsection
